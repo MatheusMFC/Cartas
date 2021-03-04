4207ff5198346ed9902bf515caf58f4c12e24500
matheus.fischer <matheus.fischer@aluno.ufabc.edu.br>
2021-03-04 07:55:01 -0300

    Primeiro commit do Lab_Cartas

---
+++ Assets/Scripts/ManageCartas.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageCartas : MonoBehaviour
+{
+
+    public GameObject carta;            //carta a ser descartada
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MostraCartas();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MostraCartas()
+    {
+        //Instantiate(carta, new Vector3(0, 0, 0), Quaternion.identity);
+        //AddUmaCarta();
+        for (int i = 0; i < 13; i++)
+        {
+            AddUmaCarta(i);
+        }
+    }
+
+    void AddUmaCarta(int rank)
+    {
+        GameObject centro = GameObject.Find("centroDaTela");
+        float escalaCartaOriginal = carta.transform.localScale.x;
+        float fatorEscalaX = (650 * escalaCartaOriginal) / 100.0f;
+        //Vector3 novaPosicao = new Vector3(centro.transform.position.x + ((rank - 13 / 2) * 1.3f), centro.transform.position.y, centro.transform.position.z);
+        Vector3 novaPosicao = new Vector3(centro.transform.position.x + ((rank - 13 / 2) * fatorEscalaX), centro.transform.position.y, centro.transform.position.z);
+        //GameObject c = (GameObject)(Instantiate(carta, new Vector3(0, 0, 0), Quaternion.identity));
+        //GameObject c = (GameObject)(Instantiate(carta, new Vector3(rank*2.0f, 0, 0), Quaternion.identity));
+        GameObject c = (GameObject)(Instantiate(carta, novaPosicao , Quaternion.identity));
+        c.tag = "" + rank;
+        c.name = "" + rank;
+        string nomeDaCarta = "";
+        string numeroCarta = "";
+        switch (rank)
+        {
+            case 0:
+                numeroCarta = "ace";
+                break;
+            case 10:
+                numeroCarta = "jack";
+                break;
+            case 11:
+                numeroCarta = "queen";
+                break;
+            case 12:
+                numeroCarta = "king";
+                break;
+            default:
+                numeroCarta = "" + (rank + 1);
+                break;
+        }
+        nomeDaCarta = numeroCarta + "_of_clubs";
+        Sprite s1 = (Sprite)Resources.Load<Sprite>(nomeDaCarta);
+        print("S1: " + s1);
+        GameObject.Find("" + rank).GetComponent<Tile>().SetCartaOriginal(s1);
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    private bool tileRevelada = false;      //indicador da carta virada ou não
+    public Sprite originalCarta;            //Sprite da carta desejada
+    public Sprite backCarta;                //Sprie do avesso da carta
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EscondeCarta();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        print("Você pressinonou num Tile");
+        if (tileRevelada)
+            EscondeCarta();
+        else
+            RevelaCarta();
+    }
+
+    public void EscondeCarta()
+    {
+        GetComponent<SpriteRenderer>().sprite = backCarta;
+        tileRevelada = false;
+    }
+
+    public void RevelaCarta()
+    {
+        GetComponent<SpriteRenderer>().sprite = originalCarta;
+        tileRevelada = true;
+    }
+
+    public void SetCartaOriginal(Sprite novaCarta)
+    {
+        originalCarta = novaCarta;
+    }
+}
